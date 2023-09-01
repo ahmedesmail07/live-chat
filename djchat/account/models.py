f4983cee4852a2f79a03303bc4473c087018b44b
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-09-01 15:31:44 +0300

    Account-Account_Model

---
+++ djchat/account/models.py
@@ -0,0 +1,7 @@
+# from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Creating the user model
+
+
+class Account(AbstractUser):
+    pass
--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -8,19 +8,12 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -32,6 +25,8 @@ INSTALLED_APPS = [
     # Third party applications
     "rest_framework",
     # Local applications
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +111,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
