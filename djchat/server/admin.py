c8b9693a8ab0228689a0b3aa6b610ff21b59fa5a
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-09-04 18:01:33 +0300

    database-relationships&models

--- djchat/server/admin.py
+++ djchat/server/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Server, Channel
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ["name", "owner", "category"]
+
+
+admin.site.register(Server, ServerAdmin)
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ["name", "owner", "server"]
+
+
+admin.site.register(Channel, ChannelAdmin)
--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+    # meaning that when the admin try to add a Category it's allowed to not add desc.
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=80)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="server_category"
+    )
+    description = models.TextField(blank=True, null=True)
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=80)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
+    topic = models.CharField(max_length=200)
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channel_server"
+    )
+
+    # overriding save method to store the name in lower case
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
