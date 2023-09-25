b6b143d2a9400b7df1c88e1729e0f7ea63314c66
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-09-25 16:51:28 +0300

    filtering_by:quantity, category_name|id and user_id

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -2,23 +2,44 @@ from rest_framework import viewsets
 from .models import Server
 from .serializers import ServerSerializer
 from rest_framework.response import Response
+from django.db.models import Q
 
 
 class ServerListViewSet(viewsets.ViewSet):
-    # Initialize the queryset with all Server objects
+    """
+    A ViewSet for interacting with Server objects.
+
+    Provides list functionality with optional filters and quantity limit.
+    """
+
     queryset = Server.objects.all()
 
     def list(self, request):
-        # Get the 'category' parameter from the request's query parameters
+        """
+        Get a list of servers with optional filters based on category, user, and quantity.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: Serialized data of servers based on applied filters and quantity.
+        """
         category = request.query_params.get("category")
+        quantity = request.query_params.get("quantity")
+        by_user = request.query_params.get("by_user") == "true"
 
-        # Check if 'category' parameter is provided in the request
         if category:
-            # Filter the queryset based on the 'category' parameter by name
-            self.queryset = self.queryset.filter(category__name__icontains=category)
+            self.queryset = self.queryset.filter(
+                Q(category__name__icontains=category)
+                | Q(category__id__icontains=category)
+            )
 
-        # Serialize the filtered queryset to prepare the response data
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
+
+        if quantity:
+            self.queryset = self.queryset[: int(quantity)]
+
         serializer = ServerSerializer(self.queryset, many=True)
-
-        # Return a Response containing the serialized data
         return Response(serializer.data)
