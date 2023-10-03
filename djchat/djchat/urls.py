7e5dc020281d73d34761f0b4e4cdb6d9bbf4898d
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-10-03 17:12:19 +0300

    image-handling

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import (
@@ -19,3 +21,6 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
